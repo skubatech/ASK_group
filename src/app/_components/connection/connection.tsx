93c070c5331d5efe20898355604a94985fe65372
ChrisYuneva <christina_yuneva@mail.ru>
2024-01-30 16:50:26 +0400

    fixed connection and contact components

--- src/app/_components/connection/connection.tsx
+++ src/app/_components/connection/connection.tsx
@@ -2,6 +2,8 @@ import styles from './connection.module.scss';
 
 import Phone from '@/assets/icons/whitePhone.svg';
 import Messenger from '@/assets/icons/messenger.svg';
+import WhatsAppWhite from '@/assets/icons/waWhite.svg';
+import TelegramWhite from '@/assets/icons/telegramWhite.svg';
 
 export const Connection = () => {
   return (
@@ -11,16 +13,36 @@ export const Connection = () => {
       </span>
       <div className={styles.itemsWrap}>
         <a href='tel:+79817272909' className={styles.item}>
-          Рассчитать заказ в звонке
+          Позвонить
           <Phone />
         </a>
+        <div className={styles.rowWrap}>
+          <a
+            className={`${styles.item} ${styles.itemRow}`}
+            href='https://web.whatsapp.com/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            WhatsApp
+            <WhatsAppWhite />
+          </a>
+          <a
+            className={`${styles.item} ${styles.itemRow}`}
+            href='https://telegram.org/'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            Telegram
+            <TelegramWhite />
+          </a>
+        </div>
         <a
           href='https://api.whatsapp.com/'
           target='_blank'
           rel='noopener noreferrer'
           className={styles.item}
         >
-          Рассчитать заказ в мессенджере
+          Оставить заяку на сайте
           <Messenger />
         </a>
       </div>
