e6c13ee901349e3c89d6dd36e834a61d2e7b2f79
ChrisYuneva <christina_yuneva@mail.ru>
2024-02-08 19:35:19 +0400

    added form

--- src/app/_components/contacts/contacts.tsx
+++ src/app/_components/contacts/contacts.tsx
@@ -9,8 +9,16 @@ import Phone from '@/assets/icons/whitePhone.svg?url';
 import Messenger from '@/assets/icons/messenger.svg?url';
 import WhatsAppWhite from '@/assets/icons/waWhite.svg?url';
 import TelegramWhite from '@/assets/icons/telegramWhite.svg?url';
+import { useState } from 'react';
+import Form from '../form/form';
 
 export const Contacts = () => {
+  const [openForm, setOpenForm] = useState(false);
+
+  const visibleForm = () => {
+    setOpenForm((prev) => !prev);
+  };
+
   return (
     <section className={`${styles.contacts} container`} id='contacts'>
       <h2 className={styles.title}>Контакты</h2>
@@ -35,7 +43,7 @@ export const Contacts = () => {
               rel='noopener noreferrer'
               className={styles.link}
             >
-              < Image src={Telegram} alt='Icon' className={styles.icon} />
+              <Image src={Telegram} alt='Icon' className={styles.icon} />
             </a>
             <a
               href='https://web.whatsapp.com/'
@@ -59,47 +67,60 @@ export const Contacts = () => {
               rel='noopener noreferrer'
               className={styles.link}
             >
-              <Image src={Instagram} alt='Icon' className={styles.icon}/>
+              <Image src={Instagram} alt='Icon' className={styles.icon} />
             </a>
           </div>
         </div>
         <div className={styles.card}>
-          <span className={styles.title}>Закажите удобным для Вас способом</span>
-          <div className={styles.links}>
-            <a className={styles.item} href='tel:+79817272909'>
-              <span className={styles.text}>Позвонить</span>
-              <Image src={Phone} alt='Icon' className={styles.icon} />
-            </a>
-            <div className={styles.itemWrap}>
-              <a
-                className={`${styles.item} ${styles.itemRow}`}
-                href='https://web.whatsapp.com/'
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <span className={styles.text}>WhatsApp</span>
-                <Image src={WhatsAppWhite} alt='Icon' className={styles.icon} />
-              </a>
-              <a
-                className={`${styles.item} ${styles.itemRow}`}
-                href='https://telegram.org/'
-                target='_blank'
-                rel='noopener noreferrer'
-              >
-                <span className={styles.text}>Telegram</span>
-                <Image src={TelegramWhite} alt='Icon' className={styles.icon} />
-              </a>
-            </div>
-            <a
-              className={styles.item}
-              href='https://api.whatsapp.com/'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <span className={styles.text}>Оставить заяку на сайте</span>
-              <Image src={Messenger} alt='Icon' className={styles.icon} />
-            </a>
-          </div>
+          {!openForm ? (
+            <>
+              <span className={styles.title}>
+                Закажите удобным для Вас способом
+              </span>
+              <div className={styles.links}>
+                <a className={styles.item} href='tel:+79817272909'>
+                  <span className={styles.text}>Позвонить</span>
+                  <Image src={Phone} alt='Icon' className={styles.icon} />
+                </a>
+                <div className={styles.itemWrap}>
+                  <a
+                    className={`${styles.item} ${styles.itemRow}`}
+                    href='https://web.whatsapp.com/'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    <span className={styles.text}>WhatsApp</span>
+                    <Image
+                      src={WhatsAppWhite}
+                      alt='Icon'
+                      className={styles.icon}
+                    />
+                  </a>
+                  <a
+                    className={`${styles.item} ${styles.itemRow}`}
+                    href='https://telegram.org/'
+                    target='_blank'
+                    rel='noopener noreferrer'
+                  >
+                    <span className={styles.text}>Telegram</span>
+                    <Image
+                      src={TelegramWhite}
+                      alt='Icon'
+                      className={styles.icon}
+                    />
+                  </a>
+                </div>
+                <div className={styles.item} onClick={visibleForm}>
+                <span className={styles.text}>
+                    Оставить заяку на сайте
+                  </span>
+                  <Image src={Messenger} alt='Icon' className={styles.icon} />
+                </div>
+              </div>
+            </>
+          ) : (
+            <Form onClick={visibleForm}/>
+          )}
         </div>
       </div>
     </section>
